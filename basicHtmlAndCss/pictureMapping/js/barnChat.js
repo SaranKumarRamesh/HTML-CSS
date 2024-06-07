b47767cfc70f087398b83477a8129403bb87dce5
Saran Kumar Ramesh <ramesh0192@saskpolytech.ca>
2024-06-07 09:22:22 -0600

    🌐 Practiced and implemented basic HTML & CSS concepts
    - Practiced HTML including selectors, tags, linking, and connection practices
    - Practiced CSS including animations, transitions, fonts, display, grid, shadows, blocks, and positioning
    - Combined HTML and CSS to create fully functional webpages for several projects
    - Started practicing XML basics

---
+++ basicHtmlAndCss/pictureMapping/js/barnChat.js
@@ -0,0 +1,36 @@
+function moo(){
+    document.getElementById("cow").style.opacity = 100;
+}
+function moo2(){
+    document.getElementById("cow").style.opacity = 0;
+}
+function bah(){
+    document.getElementById("sheep").style.opacity = 100;
+}
+function bah2(){
+    document.getElementById("sheep").style.opacity = 0;
+}
+function cock_a_doodle_do(){
+    document.getElementById("rooster").style.opacity = 100;
+}
+function cock_a_doodle_do2(){
+    document.getElementById("rooster").style.opacity = 0;
+}
+function ruff(){
+    document.getElementById("dog").style.opacity = 100;
+}
+function ruff2(){
+    document.getElementById("dog").style.opacity = 0;
+}
+function neigh(){
+    document.getElementById("horse").style.opacity = 100;
+}
+function neigh2(){
+    document.getElementById("horse").style.opacity = 0;
+}
+function maa(){
+    document.getElementById("goat").style.opacity = 100;
+}
+function maa2(){
+    document.getElementById("goat").style.opacity = 0;
+}
